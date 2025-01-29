3bb45df4e2d09afd003bae0bdeee14652d3e9b26
Bruno Cardoso <brunocardoso@MacBook-Air-de-Bruno.local>
2025-01-29 02:46:57 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const setRoutes = require('./routes/index');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
